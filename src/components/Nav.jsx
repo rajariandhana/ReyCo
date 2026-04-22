a770bd41a2d7f2978e19b3d9ec0f9894ea1580db
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2026-04-23 09:52:38 +1000

    feat: home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router'
 
 const Nav = () => {
 	return (
-		<header className="sticky top-0 z-20 w-full px-12 py-6 justify-between flex">
+		<header className="sticky top-0 z-20 w-full px-6 md:px-12 py-6 justify-between flex">
 			<div className='flex w-1/2'>
-				<h1 className='font-garamond text-3xl'>ReyCo</h1>
+				<h1 className='font-garamond text-3xl text-rose-300'>ReyCo</h1>
 			</div>
-			<div className='flex w-1/2 justify-end gap-12 items-center'>
+			<div className='flex w-1/2 text-xs md:text-md justify-end gap-12 items-center'>
 				<Link>About</Link>
-				<Link>Our Founders</Link>
-				<Link>Contact Us</Link>
+				<Link>Founders</Link>
+				<Link>Contact</Link>
 			</div>
 		</header>
 	)
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,19 @@
 export default function Home() {
   return (
     <>
-      <h1>home</h1>
-      <div>
-        <h2>our founders</h2>
+      <div className="flex flex-col w-full gap-4">
+        <img
+          src="founders.png"
+          alt="Founders"
+          className="object-cover p-1 rounded-full size-32 md:size-48 z-0"
+        />
+        <p className="text-justify mt-4">
+          <span className="text-rose-300">ReyCo</span> is a fictional company
+          that operates in the IT field, providing services ranging from
+          creating websites, cyber security, and game development. Started by our founders, Rey and Coco, since{" "}
+          <span className="text-rose-300">1967</span> ReyCo has been trusted by
+          satisfied clients all over the world.
+        </p>
       </div>
     </>
   );
